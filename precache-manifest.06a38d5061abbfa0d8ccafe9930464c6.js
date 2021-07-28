e7cc7e70af944ae6ebf84080c3b61fa0d1422bcd
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2021-07-29 01:37:18 +0530

    Updates

---
+++ precache-manifest.06a38d5061abbfa0d8ccafe9930464c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446c2145490230a3642da71f8d83635d",
+    "url": "/slider-react/index.html"
+  },
+  {
+    "revision": "c9837666dd88a35b359a",
+    "url": "/slider-react/static/css/main.f86758df.chunk.css"
+  },
+  {
+    "revision": "e1c4e21724a78a9e8542",
+    "url": "/slider-react/static/js/2.2c3ffba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slider-react/static/js/2.2c3ffba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9837666dd88a35b359a",
+    "url": "/slider-react/static/js/main.8bfb1d23.chunk.js"
+  },
+  {
+    "revision": "d8295992b6705ab51b51",
+    "url": "/slider-react/static/js/runtime-main.1485afd0.js"
+  }
+]);
